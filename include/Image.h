93b6b9c49a25a1b007aed79e5ff602b8c011f9ac
ziaerker <ziyaerkeryenilmez@gmail.com>
2025-12-24 02:11:57 +0300

    First version

---
+++ include/Image.h
@@ -0,0 +1,25 @@
+#ifndef IMAGE_H  //CHECKS FOR Image.h DEFINED SOMEWHERE ELSE IF NOT DEFINED THEN GO NEXT LINE
+#define IMAGE_H  //DEFINE Image.h
+
+#include <string> //TO BE ABLE TO GET INPUT AND OUTPUT FILE NAMES
+
+/* I am not using namespace std; I leaved a link for that.(in readme file).
+I guess using it or not using it will not change anything at all. But my main purpose for this project to simulate sector level coding*/
+
+class Image{
+
+//no need for private if you do not write public or protected, everything is in private
+
+
+};
+
+
+
+
+
+
+
+
+
+
+#endif //END OF THE DEFINITION
